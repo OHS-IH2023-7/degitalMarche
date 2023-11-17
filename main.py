5ce63508e406edc349a8921a9581902ca3122f4e
Okinopi <114094511+okinopita@users.noreply.github.com>
2023-11-17 07:54:24 +0000

    変われええええええええええええええええええええええええ 	modified:   main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloworld():
-    return render_template("index.html")
+    #kinoshita's change
+    return 'Hello World.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
