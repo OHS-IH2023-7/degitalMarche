4c43dd85a44598e72167f6cfcf71b0f9e732aed0
taka-hal <150983667+taka-hal@users.noreply.github.com>
2023-12-07 02:56:19 +0000

    db接続しました

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from flask import Flask,render_template
+import pymysql
+
+print("動いてる")
+
+connection = pymysql.connect(
+    host="anaconda-postgres_devcontainer-mysql-1",
+    db="test",
+    user="test",
+    password="test",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+def sqlConnect(sql):
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result 
+sql = "SELECT * FROM test"
+
 app = Flask(__name__)
 
 @app.route('/')
 def helloworld():
     # return 'Hello World.'
-
-    return render_template('index.html')
+    sql = "SELECT * FROM test"
+    result = sqlConnect(sql)
+    return render_template('index.html',result=result)
 
 @app.route('/product-detail')
 def product_detail():
